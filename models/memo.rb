92eb85e752e2e6cf12d69e21c509cc6019b65046
cellotak <tak.2cellos.ashi.777@gmail.com>
2025-11-11 22:42:35 +0900

    jsonデータ読み取り後の形式をarrayからhashに変更

--- models/memo.rb
+++ models/memo.rb
@@ -7,23 +7,23 @@ class Memo
   MEMO_FILE_PATH = 'data/memo.json'
 
   def self.all
-    load_memos
+    load_memos.values
   end
 
   def self.find(id)
-    load_memos.find { |memo| memo['id'] == id.to_i }
+    load_memos[id.to_i]
   end
 
   def self.create(title:, content:)
     memos = load_memos
-    new_id = (memos.map { |memo| memo['id'] }.max || 0) + 1
+    new_id = (memos.keys.max || 0) + 1
     new_memo = {
       'id' => new_id,
       'title' => title,
       'content' => content,
       'created_at' => Time.now.iso8601
     }
-    memos << new_memo
+    memos[new_id] = new_memo
     save_memos(memos)
 
     new_memo
@@ -31,7 +31,7 @@ class Memo
 
   def self.update(id:, title:, content:)
     memos = load_memos
-    target_memo = memos.find { |memo| memo['id'] == id.to_i }
+    target_memo = memos[id.to_i]
 
     return nil unless target_memo
 
@@ -44,21 +44,22 @@ class Memo
 
   def self.destroy(id:)
     memos = load_memos
-    memos.reject! { |memo| memo['id'] == id.to_i }
+    memos.delete(id.to_i)
     save_memos(memos)
   end
 
   def self.load_memos
     if File.exist?(MEMO_FILE_PATH)
-      JSON.parse(File.read(MEMO_FILE_PATH))
+      json_data = JSON.parse(File.read(MEMO_FILE_PATH))
+      json_data.map { |memo| [memo['id'], memo] }.to_h
     else
-      []
+      {}
     end
   end
 
   def self.save_memos(memos)
     File.open(MEMO_FILE_PATH, 'w') do |file|
-      file.write(JSON.pretty_generate(memos))
+      file.write(JSON.pretty_generate(memos.values))
     end
   end
 
