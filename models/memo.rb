4e12d0a42e05e70b8d58912e98c0a05143fed645
cellotak <tak.2cellos.ashi.777@gmail.com>
2025-11-22 15:19:32 +0900

    memo.rbのリファクタリング

--- models/memo.rb
+++ models/memo.rb
@@ -7,11 +7,11 @@ class Memo
   MEMO_FILE_PATH = 'data/memo.json'
 
   def self.all
-    load_memos.values
+    load_data['memos'].values
   end
 
   def self.find(id)
-    load_memos[id.to_s]
+    load_data['memos'][id.to_s]
   end
 
   def self.create(title:, content:)
@@ -19,30 +19,30 @@ class Memo
 
     data['last_id'] = id = data['last_id'] + 1
 
-    new_memo = {
+    memo = {
       'id' => id,
       'title' => title,
       'content' => content
     }
 
-    data['memos'][id.to_s] = new_memo
+    data['memos'][id.to_s] = memo
     save_data(data)
 
-    new_memo
+    memo
   end
 
   def self.update(id, title:, content:)
     data = load_data
-    target_memo = data['memos'][id.to_s]
+    memo = data['memos'][id.to_s]
 
-    return nil unless target_memo
+    return nil unless memo
 
-    target_memo['title'] = title
-    target_memo['content'] = content
+    memo['title'] = title
+    memo['content'] = content
 
     save_data(data)
 
-    target_memo
+    memo
   end
 
   def self.destroy(id)
@@ -51,14 +51,8 @@ class Memo
     save_data(data)
   end
 
-  def self.load_memos
-    load_data['memos']
-  end
-
   def self.load_data
-    return { 'last_id' => 0, 'memos' => {} } unless File.exist?(MEMO_FILE_PATH)
-
-    JSON.parse(File.read(MEMO_FILE_PATH))
+    File.exist?(MEMO_FILE_PATH) ? JSON.parse(File.read(MEMO_FILE_PATH)) : { 'last_id' => 0, 'memos' => {} }
   end
 
   def self.save_data(data)
@@ -67,5 +61,5 @@ class Memo
     end
   end
 
-  private_class_method :load_memos, :load_data, :save_data
+  private_class_method :load_data, :save_data
 end
