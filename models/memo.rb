e13902b110fec4dfe18c0c1851decaf6415cc906
cellotak <tak.2cellos.ashi.777@gmail.com>
2025-12-02 23:39:22 +0900

    SQLをヒアドキュメント化

--- models/memo.rb
+++ models/memo.rb
@@ -9,24 +9,44 @@ class Memo
   end
 
   def self.all
-    connection.exec('SELECT id, title, content FROM memos ORDER BY id DESC').to_a
+    connection.exec(<<~SQL).to_a
+      SELECT id, title, content
+      FROM memos
+      ORDER BY id DESC
+    SQL
   end
 
   def self.find(id)
-    result = connection.exec_params('SELECT id, title, content FROM memos WHERE id = $1', [id.to_i])
+    result = connection.exec_params(<<~SQL, [id.to_i])
+      SELECT id, title, content
+      FROM memos
+      WHERE id = $1
+    SQL
     result[0] if result.any?
   end
 
   def self.create(title:, content:)
-    connection.exec_params('INSERT INTO memos (title, content) VALUES ($1, $2) RETURNING id, title, content', [title, content])[0]
+    connection.exec_params(<<~SQL, [title, content])[0]
+      INSERT INTO memos (title, content)
+      VALUES ($1, $2)
+      RETURNING id, title, content
+    SQL
   end
 
   def self.update(id, title:, content:)
-    result = connection.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3 RETURNING id, title, content', [title, content, id.to_i])
+    result = connection.exec_params(<<~SQL, [title, content, id.to_i])
+      UPDATE memos
+      SET title = $1, content = $2
+      WHERE id = $3
+      RETURNING id, title, content
+    SQL
     result[0] if result.any?
   end
 
   def self.destroy(id)
-    connection.exec_params('DELETE FROM memos WHERE id = $1', [id.to_i])
+    connection.exec_params(<<~SQL, [id.to_i])
+      DELETE FROM memos
+      WHERE id = $1
+    SQL
   end
 end
